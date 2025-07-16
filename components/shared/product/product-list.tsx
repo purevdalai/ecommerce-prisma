10e471c4a40ddcee316e99383a657f71e2480fbb
Purevdalai <purevdalai.developer@gmail.com>
2025-07-16 12:19:35 +0900

    Implementation of ProductCard

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,4 +1,14 @@
-const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?: number }) => {
+import ProductCard from "./product-card";
+
+const ProductList = ({
+	data,
+	title,
+	limit,
+}: {
+	data: any;
+	title?: string;
+	limit?: number;
+}) => {
 	const limitedData = limit ? data.slice(0, limit) : data;
 	return (
 		<div className="my-10">
@@ -6,8 +16,8 @@ const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?
 
 			{limitedData.length > 0 ? (
 				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-					{limitedData.map((product: any, index: number) => (
-						<div key={index}>{product.name}</div>
+					{limitedData.map((product: any) => (
+						<ProductCard key={product.slug} product={product} />
 					))}
 				</div>
 			) : (
