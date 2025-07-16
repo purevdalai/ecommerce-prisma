aa129eecbe9b68623bb59638742b81dd89cea937
Purevdalai <purevdalai.developer@gmail.com>
2025-07-16 11:52:49 +0900

    Implementation of Responsive Sheet Menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,6 @@
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
 	return (
@@ -21,20 +18,7 @@ const Header = () => {
 						{APP_NAME}
 					</span>
 				</div>
-				<div className="space-x-2">
-					<ModeToggle />
-					<Button asChild variant="ghost">
-						<Link href="/cart">
-							<ShoppingCart className="w-4 h-4" /> Cart
-						</Link>
-					</Button>
-					<Button asChild>
-						<Link href="/sign-in">
-							<UserIcon className="w-4 h-4" />
-							Sign In
-						</Link>
-					</Button>
-				</div>
+				<Menu />
 			</div>
 		</header>
 	);
