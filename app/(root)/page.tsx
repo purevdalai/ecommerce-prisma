17f8218d1084a04357e1c8d3e8125bf736b2b11d
Purevdalai <purevdalai.developer@gmail.com>
2025-07-16 12:11:12 +0900

    Implementation of ProductList

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 export default async function HomePage() {
-	return (  
-		<div>
-			<h1>Home page</h1>
-
-			<Button>Click me</Button>
-		</div>
+	return (
+		<>
+			<ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+		</>
 	);
 }
