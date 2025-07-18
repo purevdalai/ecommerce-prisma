48955ed757bd8ab4639f0a2ae05535421f575424
Purevdalai <purevdalai.developer@gmail.com>
2025-07-18 16:24:04 +0900

    Fix bug

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,15 +4,13 @@ import { LATEST_PRODUCT_LIMIT } from "@/lib/constants";
 
 const HomePage = async () => {
 	const data = await getLatestProducts();
-  
+
 	return (
-		<>
-			<ProductList
-				data={data}
-				title="Newest Arrivals"
-				limit={Number(LATEST_PRODUCT_LIMIT)}
-			/>
-		</>
+		<ProductList
+			data={data}
+			title="Newest Arrivals"
+			limit={Number(LATEST_PRODUCT_LIMIT)}
+		/>
 	);
 };
 export default HomePage;
