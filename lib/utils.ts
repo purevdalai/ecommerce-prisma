cf76afb6c75ca5b51cf66d833b2157de2c0e8bc9
Purevdalai <purevdalai.developer@gmail.com>
2025-07-18 12:05:16 +0900

    Zod validation & type inference

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,3 +16,9 @@ export function convertToPlainObject<T>(value: T): T {
 	});
 	return parsed;
 }
+
+// Format number with decimal places
+export function formatNumber(num: number) : string {
+  const [int, decimal] = num.toString().split('.');
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
+}
